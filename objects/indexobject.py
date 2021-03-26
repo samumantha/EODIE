4314a4c575187ee8346ae1d4fde7574757efe80d
samumantha <samantha.wittke@nls.fi>
2021-03-26 23:28:01 +0200

    index choice added

--- objects/indexobject.py
+++ objects/indexobject.py
@@ -1,12 +1,14 @@
 """
 
 class to calculate indices
+returns an indexarray based on index input
 
 TODO:
     * hardcoded stuff in config
     * more indices
 """
 import numpy as np
+import re
 np.seterr(divide='ignore', invalid='ignore')
 from bandobject import BandObject
 
@@ -15,16 +17,16 @@ class IndexObject(BandObject):
     def __init__(self, inpath, resolution):
         super().__init__(inpath)
         self.resolution = resolution 
+
+    def calculate_index(self, index):
+        default = "Unavailable index"
+        return getattr(self, 'calculate_' + index, lambda: default)()
         
 
     def calculate_ndvi(self):
 
         red = self.get_array('B04',self.resolution)
         nir = self.get_array('B08',self.resolution)
-        #bands= self.get_arrays()
-        #print(len(bands))
-        #red = bands[0]
-        #nir = bands[1]
 
         up = nir-red
         down = nir+red
@@ -68,3 +70,16 @@ class IndexObject(BandObject):
 
 
 
+"""
+
+#Note: next: index name as user input (eg dictionary[key] = run_keyindex(), one or more, run everything through for every index,
+advanced: bands in configfile so that eg ndvi can be run with b8a instead of b8,
+more advanced: own functions)
+
+#not needed:
+    def calculate_index(self, formula):
+        p = re.compile('B[0,1][0-9]')
+        bands = np.unique(p.findall(fomula))
+        # how to get these into variables for the formula??
+        return ne.evaluate(formula)
+"""
