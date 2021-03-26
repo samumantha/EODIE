c70dd9ce1283b9ea070f31169185bde6dab852f6
samumantha <samantha.wittke@nls.fi>
2021-03-26 23:27:14 +0200

    prints to logger

--- objects/geometryobject.py
+++ objects/geometryobject.py
@@ -3,14 +3,15 @@
 class to handle everything regarding the vectordata, atm only ESRI shapefile
 
 TODO:
-    * no fileoutput from reprojection
+    
 """
 import os
-from osgeo import gdal,osr
+from osgeo import osr
 import fiona
 import subprocess
 from copy import deepcopy
 from shapely.geometry import Polygon
+import logging
 
 class GeometryObject(object):
 
@@ -41,23 +42,20 @@ class GeometryObject(object):
         return epsgcode
 
     def reproject_to_epsg(self, myepsg):
-        print('INFO: checking the projection of the inputfile now')
+        logging.info('Checking the projection of the inputfile now')
         epsgcode = self.get_epsg()
         head,tail,root,ext = self.split_path()
 
         if epsgcode == myepsg:
-            print('INFO: input shapefile has EPSG ' + epsgcode + ' that works!')
+            logging.info('Input shapefile has EPSG {} that works!'.format(epsgcode))
         else:
             reprojectedshape = os.path.join(head, root + '_reprojected_' + myepsg +  ext)
             if not os.path.exists(reprojectedshape):
                 reprojectcommand = 'ogr2ogr -t_srs EPSG:' + myepsg + ' ' +  reprojectedshape + ' ' + self.geometries
-                print(reprojectcommand)
+                logging.info('Reprojectcommand: {}'.format(reprojectcommand))
                 subprocess.call(reprojectcommand, shell=True)
-                print('INFO: input shapefile had other than EPSG ' + myepsg + ' but was reprojected and works now')
+                logging.info('input shapefile had other than EPSG {} but was reprojected and works now'.format(myepsg))
             self.geometries = reprojectedshape
-
-
-        
 
     def get_properties(self):
         with fiona.open(self.geometries,'r') as opengeom:
@@ -69,6 +67,6 @@ class GeometryObject(object):
     def get_boundingbox(self):
         with fiona.open(self.geometries,'r') as open_shp:
             bounding_box_coordinates = open_shp.bounds
-            print(bounding_box_coordinates)
+            logging.info(bounding_box_coordinates)
         return Polygon.from_bounds(bounding_box_coordinates[0], bounding_box_coordinates[1], bounding_box_coordinates[2], bounding_box_coordinates[3] )
     
