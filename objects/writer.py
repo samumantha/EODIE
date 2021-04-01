1ca4c7e745c5138acf4edeeec5438a9f05f3e4ac
samumantha <samantha.wittke@nls.fi>
2021-04-01 16:41:51 +0300

    pickle writer added

--- objects/writer.py
+++ objects/writer.py
@@ -10,17 +10,18 @@ TODO:
 import os
 import csv
 import logging
+import pickle
 
 class WriterObject(object):
 
     def __init__(self,outdir, date, tile, extractedarrays, index, statistics):
-        self.outpath = os.path.join(outdir ,index+ '_statistics_' + date +'_'+ tile + '.csv')
+        self.outpath = os.path.join(outdir ,index+ '_statistics_'+ date +'_'+ tile)
         self.extractedarrays = extractedarrays
         self.statistics = statistics
 
     def write_csv(self):
-    
-        logging.info(self.outpath)
+        self.outpath = self.outpath + '_stat.csv'
+        logging.info('stat to csv in: ' + self.outpath)
         with open(self.outpath, mode='w') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=',')
             csv_writer.writerow(['id']+ self.statistics )
@@ -28,8 +29,23 @@ class WriterObject(object):
                 onerow = [key] + self.extractedarrays[key]
                 csv_writer.writerow(onerow)
 
-    def write_csv_arr(self):
+    def write_pickle_arr(self):
+        self.outpath = self.outpath + '_array'
+        logging.info('arrays to pickle in: ' + self.outpath)
+        with open(self.outpath, mode='wb') as pkl_file:
+            pickle.dump(self.extractedarrays,pkl_file)
 
+
+    def write_csv_arr(self):
+        self.outpath = self.outpath + '_array.csv'
+        logging.info('arrays to csv in: ' + self.outpath)
         with open(self.outpath, 'w') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerows(self.extractedarrays)
+            for key, value in self.extractedarrays.items():
+                writer.writerow([key,value])
+            """
+            for key in self.extractedarrays.keys():
+                onerow = [key] + self.extractedarrays[key]
+                writer.writerow(onerow)
+            """
+            
