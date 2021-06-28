378f6d5fd6c898d9e683bcebde09f816a889235e
Petteri <petteri.lehti@maanmittauslaitos.fi>
2021-06-28 13:01:53 +0300

    timeseries bug fixes and comments

--- depickler.py
+++ depickler.py
@@ -27,7 +27,7 @@ parser.add_argument('--end', dest='enddate', default='20201231', help='end date 
 parser.add_argument('--id', dest='id', default=['all'], nargs='*', help='ID or list of IDs of the field parcel we want to plot')
 parser.add_argument('--show', dest='show', default='0', help='1 for opening each figure window, default is 0 to not open')
 parser.add_argument('--cmap', dest='cmap', default='viridis', help='which colormap/scale to use, viridis by default')
-parser.add_argument('--series', dest='timeseries', default='1', help='1 if you want timeseries over whole timeperiod')
+parser.add_argument('--series', dest='timeseries', default='0', help='1 if you want timeseries over whole timeperiod')
 input = parser.parse_args()
 
 my_cmap = copy(cm.get_cmap(input.cmap)) # Getting (and copying) the wanted colormap
@@ -41,7 +41,7 @@ f = open(input.lookup_table, "r")
 table = f.read().splitlines()
 f.close()
 
-if not int(input.timeseries):
+if not int(input.timeseries): # Individual plots for each timepoint
     # Finding the wanted tiles based on --id input and the lookup table:
     tiles=[]
     if 'all' in input.id:
@@ -79,56 +79,69 @@ if not int(input.timeseries):
                         plt.close()
 
 
-elif int(input.timeseries):
-    wantedIDs = input.id
-    if 'all' in input.id:
+elif int(input.timeseries): # Timeseries where id and index stay the same
+    wantedIDs = input.id 
+    if 'all' in input.id: # If input is 'all', we want all the IDs available
         wantedIDs=[]
         for line in table:
-            wantedIDs.extend(line.split(":")[1].split(","))
-    wantedIDs = list(dict.fromkeys(wantedIDs))
-    for id in wantedIDs:
+            wantedIDs.extend(line.split(":")[1].split(",")) # Every ID extracted from lookup table
+    wantedIDs = list(dict.fromkeys(wantedIDs)) # Removes duplicates
+
+    for wantedID in wantedIDs:
         tiles = []
         for line in table:
                 idlist = line.split(":")[1].split(",")
-                if id in idlist:
-                    tiles.append(line.split(":")[0])
+                if wantedID in idlist:
+                    tiles.append(line.split(":")[0]) # Add each tile with wanted IDs
+        
         wantedIndices = input.index
-        if 'all' in input.index:
+        if 'all' in input.index: # If input is 'all', we want all indices available
             wantedIndices=[]
             for entry in os.scandir(input.mydir):
                 fileFinder = FileFinder(entry.name)
                 if(entry.is_file() and fileFinder.check_file(input.index, input.startdate, input.enddate, tiles)):
-                    wantedIndices.append(fileFinder.index)
-            wantedIndices = list(dict.fromkeys(wantedIndices)) #Removes duplicates :D
+                    wantedIndices.append(fileFinder.index) #Add index to list for each wanted file
+            wantedIndices = list(dict.fromkeys(wantedIndices)) # Removes duplicates
+
         for index in wantedIndices:
             files = []
             for entry in os.scandir(input.mydir):
                 fileFinder = FileFinder(entry.name)
                 if(entry.is_file() and fileFinder.check_file([index], input.startdate, input.enddate, tiles)):
-                    files.append(entry)
-            grid_size = len(files)
-            grid_width = int(np.floor(np.sqrt(grid_size)))
-            grid_height = int(np.ceil(np.divide(grid_size, grid_width)))
-            fig, axs = plt.subplots(grid_height, grid_width)
-            title = index + '_' + id
-            plt.suptitle(title)
-            grid_index = 0
-            files = sorted(files, key=lambda x: x.name.split("_")[1])
-            for file in files:
-                infile = open(file, 'rb')
-                new_dict = pickle.load(infile)
-                infile.close()
-                for id_key in new_dict:
-                    if str(id_key) == id:
-                        pickle_arr = new_dict[id_key]
-                plt.subplot(grid_height, grid_width, grid_index + 1)
-                plt.imshow(pickle_arr, norm=my_norm, cmap=my_cmap)
-                plt.title(file.name.split("_")[1])
-                plt.subplot(grid_height, grid_width, grid_index + 1).set_xticks([]) # Getting rid of axis ticks
-                plt.subplot(grid_height, grid_width, grid_index + 1).set_yticks([])
-                grid_index += 1
-            plt.show()
-            plt.close()
+                    files.append(entry) # Add all the files with correct index and ID to a list 
+            if len(files): # Try to loop thorugh them only if the list is not empty
+                grid_size = len(files) # Needed grid size is amount of files
+                grid_width = int(np.floor(np.sqrt(grid_size))) # This will make width less than or equal to height
+                grid_height = int(np.ceil(np.divide(grid_size, grid_width)))
+                fig, axs = plt.subplots(grid_height, grid_width)
+                title = index + '_' + wantedID
+                plt.suptitle(title)
+
+                grid_index = 1 # We start from subplot 1 (indexing starts from 1 here because of how plt.subplot works)
+                files = sorted(files, key=lambda x: x.name.split("_")[1]) # Sort the list of files based on time
+                for file in files:
+                    infile = open(file, 'rb')
+                    new_dict = pickle.load(infile)
+                    infile.close()
+                    for id_key in new_dict:
+                        if str(id_key) == wantedID:
+                            pickle_arr = new_dict[id_key] # find correct array based on wantedID
+                    plt.subplot(grid_height, grid_width, grid_index) # Plotting in the correct spot
+                    img = plt.imshow(pickle_arr, norm=my_norm, cmap=my_cmap)
+                    plt.title(file.name.split("_")[1]) # Title for each subplot is just the time
+                    plt.subplot(grid_height, grid_width, grid_index).set_xticks([]) # Getting rid of axis ticks
+                    plt.subplot(grid_height, grid_width, grid_index).set_yticks([])
+                    grid_index += 1 # Increment index to plot the next subplot in the correct spot
+
+                # Adding colorbar:
+                fig.subplots_adjust(right=0.8)
+                cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+                fig.colorbar(img, cax=cbar_ax)
+                
+                if int(input.show):
+                    plt.show()
+                fig.savefig(os.path.join(input.outdir, title + "_timeseries.png"))
+                plt.close()
 
 
 
