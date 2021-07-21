1ec807a126449348fdb901158cb3c652020e92ca
Petteri <petteri.lehti@maanmittauslaitos.fi>
2021-07-21 16:33:08 +0300

    made shapesplitter object and automated splitshp

---
+++ objects/splitshp.py
@@ -0,0 +1,144 @@
+import os
+import sys
+import fiona
+import multiprocessing as mp
+import shapely
+from shapely.geometry import shape, Polygon
+from copy import deepcopy
+from osgeo import osr
+import subprocess
+import glob
+import logging
+
+class SplitshpObject(object):
+
+    def __init__(self, small_polygon_shapefile, large_polygon_shapefile, output_directory):
+        #all input shapefiles to EPSG: 4326
+        self.small_polygon_shapefile = self.reproject_to_epsg(small_polygon_shapefile,'4326')
+        self.large_polygon_shapefile = self.reproject_to_epsg(large_polygon_shapefile,'4326')
+        self.output_directory = output_directory
+        self.tiles = []
+
+    def reproject_to_epsg(self, myshp,myepsg):
+        logging.info('checking the projection of the inputfile now')
+        head, tail = os.path.split(myshp)
+        root, ext = os.path.splitext(tail)
+        rootprj = root + '.prj'
+        projectionfile = os.path.join(head, rootprj)
+        prj_file = open(projectionfile , 'r')
+        prj_text = prj_file.read()
+        srs = osr.SpatialReference()
+        srs.ImportFromESRI([prj_text])
+        srs.AutoIdentifyEPSG()
+        epsgcode = srs.GetAuthorityCode(None)
+        if epsgcode == myepsg:
+            logging.info('input shapefile has EPSG '  +myepsg + ' that works!')
+            return myshp
+        else:
+            reprojectedshape = os.path.join(head, root + '_reprojected_' + myepsg +  ext)
+            if not os.path.exists(reprojectedshape):
+                reprojectcommand = 'ogr2ogr -t_srs EPSG:' + myepsg + ' ' +  reprojectedshape + ' ' + myshp
+                #print(reprojectcommand)
+                subprocess.call(reprojectcommand, shell=True)
+                logging.info('input shapefile had other than EPSG ' + myepsg + ' but was reprojected and works now')
+            return reprojectedshape
+
+    def get_parameter_content(self, polygon, parameter):
+        return polygon[parameter]
+
+    def make_geometryobject(self, parameter_content):
+        return shape(parameter_content)
+
+    def get_shp_properties(self, shapefile):
+        driver = shapefile.driver
+        schema = deepcopy(shapefile.schema)
+        crs = shapefile.crs
+        return driver,schema,crs
+
+    def get_bounding_box(self, shapefile):
+        with fiona.open(shapefile,'r') as open_shp:
+            bounding_box_coordinates = open_shp.bounds
+            #print(bounding_box_coordinates)
+        return Polygon.from_bounds(bounding_box_coordinates[0], bounding_box_coordinates[1], bounding_box_coordinates[2], bounding_box_coordinates[3] )
+
+
+    def write_splitted_shapefiles(self, bounding_box_small_poly_shp,tile,outshpdir, small_poly_shp):
+        one_tile_geometry = self.make_geometryobject(self.get_parameter_content(tile,'geometry')) #on puhti: TypeError: 'NoneType' object is not subscriptable
+        if one_tile_geometry.intersects(bounding_box_small_poly_shp):
+            tilename = tile['properties']['Name']
+
+            originalname = os.path.splitext(os.path.split(small_poly_shp)[-1])[0]
+            outshpname = os.path.join(outshpdir,originalname + '_' + str(tilename)+'.shp')
+
+            with fiona.open(small_poly_shp,'r') as smaller_open_shp:
+                driver,schema,crs = self.get_shp_properties(smaller_open_shp)
+
+                # open the new shapefile 
+                with fiona.open(outshpname, 'w', driver,schema,crs) as outputshp:
+                    for small_polygon in smaller_open_shp:
+                        small_polygon_geometry = self.get_parameter_content(small_polygon, 'geometry')
+                        if not small_polygon_geometry is None:
+                            #only if there is the polygon is intersecting the tile in question, it will be written to the new file
+                            # this is the point where there may be issues if the polygon is actually a multipolygon
+                            # handling multipolygons at this stage is out of scope of the code at this point so not supported
+                            #if shape(small_polygon['geometry']).within(one_tile_geometry):
+                            if self.make_geometryobject(small_polygon_geometry).intersects(one_tile_geometry):                
+                                outputshp.write({                                 
+                                    'properties': small_polygon['properties'], 
+                                    'geometry': small_polygon['geometry']})
+                                    
+                # if the shp is only 100 bytes it is empty and can be deleted
+                if os.path.getsize(outshpname) <= 100:
+                    filename = os.path.splitext(outshpname)[0]
+                    for ext in ['.shp','.shx','.dbf','.prj','.cpg']:
+                        if os.path.exists(filename + ext):
+                            os.remove(filename + ext)
+
+
+    def extract_needed_tiles(self, small_poly_shp, large_poly_shp, outshpname):
+
+        bounding_box_small_poly_shp = self.get_bounding_box(small_poly_shp)
+
+        with fiona.open(large_poly_shp,'r') as s2shp:
+            driver,schema,crs = self.get_shp_properties(s2shp)
+
+            with fiona.open(outshpname, 'w', driver,schema,crs) as outputshp:
+                for tile in s2shp:
+                    one_tile_geometry = self.make_geometryobject(self.get_parameter_content(tile,'geometry'))
+                    if one_tile_geometry.intersects(bounding_box_small_poly_shp):
+                        outputshp.write({                                 
+                            'properties': tile['properties'], 
+                            'geometry': tile['geometry']})
+
+
+    def splitshp(self):
+        self.splitshp_world()
+        self.splitshp_mp()
+        large_polygon_filename = os.path.splitext(self.large_polygon_shapefile)[0]
+        removelist = (glob.glob(large_polygon_filename + '.*'))
+        for item in removelist:
+            os.remove(item)
+        logging.info('splitted shapefiles now exist')
+
+    def splitshp_world(self):
+        #build the output name from both input files
+        self.out_shape_name = os.path.join(self.output_directory, os.path.splitext(os.path.split(self.large_polygon_shapefile)[-1])[0] + '_' + os.path.split(self.small_polygon_shapefile)[-1] )
+
+        self.extract_needed_tiles(self.small_polygon_shapefile, self.large_polygon_shapefile, self.out_shape_name)
+
+    def splitshp_mp(self):
+        self.large_polygon_shapefile = self.reproject_to_epsg(self.out_shape_name,'4326')
+
+        bounding_box_small_poly_shp = self.get_bounding_box(self.small_polygon_shapefile)
+
+        usable_number_of_cores = mp.cpu_count()-2
+        logging.info('number of usable cores for shapesplitting is ' + str(usable_number_of_cores))
+
+        pool = mp.Pool(usable_number_of_cores)
+
+        with fiona.open(self.large_polygon_shapefile,'r') as s2shp:
+            for tile in s2shp:
+                self.tiles.append(tile['properties']['Name'])
+                pool.apply_async(self.write_splitted_shapefiles, args = (bounding_box_small_poly_shp, tile, self.output_directory, self.small_polygon_shapefile))
+            pool.close()
+            pool.join()
--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import argparse
 import re
 import sys
 import os
+import fiona
 sys.path.append("./objects")
 from extractor import Extractor
 from cloudobject import CloudObject
@@ -12,6 +13,7 @@ from pathfinder import Pathfinder
 from raster_validator import RasterValidator
 from writer import WriterObject
 from userinput import UserInput
+from splitshp import SplitshpObject
 import logging
 from datetime import datetime 
 import yaml
@@ -29,61 +31,73 @@ if not os.path.exists('./results'):
 #setup logging 
 logging.basicConfig(filename=os.path.join(userinput.outpath, datetime.now().strftime("%Y%m%d-%H%M%S") + '.log'), level=logging.INFO)
 
-#running through either one file, if file was given or multiple files if dir was given
-for path in userinput.input:
 
-    pathfinderobject = Pathfinder(path)
-    
-    logging.info('Imagepath is {}'.format(pathfinderobject.imgpath))
-    logging.info('Tile is {}'.format(pathfinderobject.tile))
-    logging.info('Date is {}'.format(pathfinderobject.date))
-    
+#Read userinput.shpbase and worldtiles, do splitshp_world, then splitshp_mp and give new shapefile(s?) to next step. Loop in case of many shapefiles?
+small_polygon_shapefile = userinput.shpbase + '.shp'
+world_tiles = 'shp_preparation/sentinel2_tiles_world/sentinel2_tiles_world.shp'
+shp_directory = os.path.dirname(small_polygon_shapefile)
+shapesplitter = SplitshpObject(small_polygon_shapefile, world_tiles, shp_directory)
+shapesplitter.splitshp()
+for split_tile in shapesplitter.tiles:
+    #running through either one file, if file was given or multiple files if dir was given
+    for path in userinput.input:
 
-    if int(pathfinderobject.date) <= int(userinput.enddate) and int(pathfinderobject.date) >= int(userinput.startdate):
-    
-        cloudobject = CloudObject(pathfinderobject.imgpath)
-        cloudmask = cloudobject.create_cloudmask()
-        logging.info('Shape of cloudmask is {}'.format(cloudmask.shape))
-        indexobject = IndexObject(pathfinderobject.imgpath,cfg['resolution'])
-        geoobject = Geometry(userinput.shpbase + '_' + pathfinderobject.tile +'.shp')
-        geoobject.reproject_to_epsg(indexobject.epsg)
-        shapefile = geoobject.geometries
-
-        maxcloudcover = cfg['maxcloudcover']
-        rastervalidatorobject = RasterValidator(path, maxcloudcover, geoobject)
-        logging.info('Cloudcover below {}: {}'.format(maxcloudcover, rastervalidatorobject.cloudcovered))
-        logging.info('Data withing area of interest: {}'.format(rastervalidatorobject.datacovered))
+        pathfinderobject = Pathfinder(path)
         
-        if rastervalidatorobject.cloudcovered and rastervalidatorobject.datacovered:
+        logging.info('Imagepath is {}'.format(pathfinderobject.imgpath))
+        logging.info('Tile is {}'.format(pathfinderobject.tile))
+        logging.info('Date is {}'.format(pathfinderobject.date))
+        logging.info('Shapefile tile is {}'.format(split_tile))
 
-            for index in userinput.indexlist:
-                # check if bands are re
-                if index in indexobject.supportedindices:
-                    array = indexobject.calculate_index(index)
-                elif re.match('B[0-1]\d', index) or index == 'B8A':
-                    array = indexobject.get_band(index)
-                else:
-                    logging.warning('Chosen index {} not available, continuing with next index.'.format(index))
-                    
-                affine = indexobject.affine
+        if int(pathfinderobject.date) <= int(userinput.enddate) and int(pathfinderobject.date) >= int(userinput.startdate) and split_tile == pathfinderobject.tile:
+        
+            cloudobject = CloudObject(pathfinderobject.imgpath)
+            cloudmask = cloudobject.create_cloudmask()
+            logging.info('Shape of cloudmask is {}'.format(cloudmask.shape))
+            indexobject = IndexObject(pathfinderobject.imgpath,cfg['resolution'])
+            try:
+                geoobject = Geometry(userinput.shpbase + '_' + split_tile + '.shp')
+                geoobject.reproject_to_epsg(indexobject.epsg)
+            except FileNotFoundError:
+                geoobject = Geometry(userinput.shpbase + '_reprojected_4326_' + split_tile + '.shp')
+                geoobject.reproject_to_epsg(indexobject.epsg)
+            shapefile = geoobject.geometries
 
-                if int(userinput.stat) == 1: 
-                    logging.info('Statistics: {}'.format(pathfinderobject.tile))
-                    extractorobject = Extractor(cloudmask, array, shapefile, userinput.idname,affine, userinput.statistics)
-                    extractedarray = extractorobject.extract_arrays_stat()
-                    writerobject = WriterObject(userinput.outpath, pathfinderobject.date, pathfinderobject.tile, extractedarray, index, userinput.statistics)
-                    extractorobject.extract_arrays_stat()
-                    writerobject.write_csv()
-                    
-
-                elif int(userinput.stat) == 0:
-                    
-                    extractorobject = Extractor(cloudmask, array, shapefile, userinput.idname,affine,['count'])
-                    extractedarray = extractorobject.extract_arrays()
-                    writerobject = WriterObject(userinput.outpath, pathfinderobject.date, pathfinderobject.tile, extractedarray, index, ['array'])
-                    extractorobject.extract_arrays()
-                    writerobject.write_pickle_arr()
+            maxcloudcover = cfg['maxcloudcover']
+            rastervalidatorobject = RasterValidator(path, maxcloudcover, geoobject)
+            logging.info('Cloudcover below {}: {}'.format(maxcloudcover, rastervalidatorobject.cloudcovered))
+            logging.info('Data withing area of interest: {}'.format(rastervalidatorobject.datacovered))
             
-        else:
-            logging.warning('Cloudcovered or no data in Area of interest!')
-            
+            if rastervalidatorobject.cloudcovered and rastervalidatorobject.datacovered:
+
+                for index in userinput.indexlist:
+                    # check if bands are re
+                    if index in indexobject.supportedindices:
+                        array = indexobject.calculate_index(index)
+                    elif re.match('B[0-1]\d', index) or index == 'B8A':
+                        array = indexobject.get_band(index)
+                    else:
+                        logging.warning('Chosen index {} not available, continuing with next index.'.format(index))
+                        
+                    affine = indexobject.affine
+
+                    if int(userinput.stat) == 1: 
+                        logging.info('Statistics: {}'.format(pathfinderobject.tile))
+                        extractorobject = Extractor(cloudmask, array, shapefile, userinput.idname,affine, userinput.statistics)
+                        extractedarray = extractorobject.extract_arrays_stat()
+                        writerobject = WriterObject(userinput.outpath, pathfinderobject.date, pathfinderobject.tile, extractedarray, index, userinput.statistics)
+                        extractorobject.extract_arrays_stat()
+                        writerobject.write_csv()
+                        
+
+                    elif int(userinput.stat) == 0:
+                        
+                        extractorobject = Extractor(cloudmask, array, shapefile, userinput.idname,affine,['count'])
+                        extractedarray = extractorobject.extract_arrays()
+                        writerobject = WriterObject(userinput.outpath, pathfinderobject.date, pathfinderobject.tile, extractedarray, index, ['array'])
+                        extractorobject.extract_arrays()
+                        writerobject.write_pickle_arr()
+                
+            else:
+                logging.warning('Cloudcovered or no data in Area of interest!')
+                
