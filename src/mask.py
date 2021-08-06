66d8cf9ce8453bc1c42b9693556958ee427a71ce
samumantha <samantha.wittke@nls.fi>
2021-08-06 16:57:17 +0300

    minimal cloudmask check

--- src/mask.py
+++ src/mask.py
@@ -13,13 +13,13 @@ class Mask(RasterData):
     def __init__(self,inpath, configfile, test, external=None):
         super().__init__(inpath,configfile, test)
         if external is not None:
-            self.load_cloudmask(external)
+            self.cloudmask = self.load_binary_mask(external)
         #loading config file
         with open(configfile, "r") as ymlfile:
             self.cfg = yaml.safe_load(ymlfile)
 
     def load_binary_mask(self,external):
-        """ Loads an external mask that needs to be a rasterfile with pixelsize and overlap exactly with the file to be masked """
+        """ Loads an external mask that needs to be a rasterfile with one/True is cloud, 0/False no cloud, with pixelsize as given in cfg and overlap exactly with the file to be masked """
         with rasterio.open(external) as f:
             return np.array(f.read(1)).astype(int)
 
@@ -28,13 +28,6 @@ class Mask(RasterData):
         
         tobemaskedlist = self.cfg['tobemaskedlist']
         mask = np.isin(sclarray, tobemaskedlist)
-        """
-        # one is cloud, 0 no cloud
-        newmask = np.logical_not(mask)
-        cloudarray = np.full(sclarray.shape,1)
-        cloudarray[newmask] = 0
-        return cloudarray
-        """
         return mask
         
 
