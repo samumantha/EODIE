f5454726bb7c8b29806773f26a65c89005b1d9aa
Petteri <petteri.lehti@maanmittauslaitos.fi>
2021-08-16 12:35:22 +0300

    commiting before rebase

--- src/mask.py
+++ src/mask.py
@@ -54,6 +54,7 @@ class Mask(RasterData):
             return np.array(f.read(1)).astype(int)
 
     def binarize_cloudmask(self,sclarray):
+<<<<<<< HEAD
         """ takes in an array with different cloud classes and binarizes it according to config file 
         to True being the to be masked pixels (eg clouds) and False for pixels that are ok to use 
         Parameters
@@ -68,6 +69,23 @@ class Mask(RasterData):
 
         tobemaskedlist = self.cfg['tobemaskedlist']
         mask = np.isin(sclarray, tobemaskedlist)
+=======
+        """ takes in an array with different cloud classes and binarizes it according to config file to True being the to be masked pixels (eg clouds) and False for pixels that are ok to use """
+        bitmask = self.cfg['bitmask']
+        tobemaskedlist = self.cfg['tobemaskedlist']
+        if not bitmask:
+            mask = np.isin(sclarray, tobemaskedlist)
+        else:
+            mask = self.createbitmask(sclarray, tobemaskedlist)
+
+        """
+        # one is cloud, 0 no cloud
+        newmask = np.logical_not(mask)
+        cloudarray = np.full(sclarray.shape,1)
+        cloudarray[newmask] = 0
+        return cloudarray
+        """
+>>>>>>> commiting before rebase
         return mask
         
 
@@ -86,3 +104,12 @@ class Mask(RasterData):
 
         return cloudmask
 
+
+    def createbitmask(self, maskarr, tobemasked):
+        return np.array(list(map(lambda row: list(map(lambda pixel: self.checkbits(pixel, tobemasked), row)), maskarr)))
+        
+    def checkbits(data, tobemaskedlist):
+        for bit in tobemaskedlist:
+            if bool(1 << bit & data):
+                return True
+        return False
