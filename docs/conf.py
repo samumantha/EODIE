a02b2960037e15c8665205d6ebe0c358974fbbc8
Samantha Wittke <samantha.wittke@nls.fi>
2021-08-18 15:21:52 +0000

    temporarily remove napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # Napoleon - for docstrings
-extensions = ['sphinxcontrib.napoleon'
-]
+extensions = []#'sphinxcontrib.napoleon']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
