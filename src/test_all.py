1d7ed717aadf3bc32dc13e56749a85c9f5b1af7a
Petteri <petteri.lehti@maanmittauslaitos.fi>
2021-08-23 16:33:19 +0300

    testing CI with all indices

--- src/test_all.py
+++ src/test_all.py
@@ -52,7 +52,7 @@ class TestAll(object):
             cfg = yaml.safe_load(ymlfile)
         inpath = 'testfiles/S2/S2B_MSIL2A_20200626T095029_N0214_R079_T34VFN_20200626T123234.SAFE/GRANULE/L2A_T34VFN_A017265_20200626T095032/IMG_DATA'
         indexobject = Index(inpath,cfg, True)
-        for index in ['ndvi','savi']:# following exceeds memory: indexobject.supportedindices:
+        for index in indexobject.supportedindices: #['ndvi','savi']:# following exceeds memory: indexobject.supportedindices:
             indexarray = indexobject.calculate_index(index)
             indexarrayshape = indexarray.shape
             rightindexarrayshape = (10980, 10980)
@@ -203,7 +203,7 @@ class TestAll(object):
         tmpdir = 'testfiles/temp'
         if not os.path.exists(tmpdir):
             os.mkdir(tmpdir)
-        shapesplitter = SplitshpObject('testfiles/shp/test_parcels_32635.shp', cfg['tileshp'], tmpdir, 'Name')
+        shapesplitter = SplitshpObject('testfiles/shp/test_parcels_32635.shp', cfg['tileshp'] + '.shp', tmpdir, 'Name')
         tmpshpdir = shapesplitter.output_directory
         assert os.path.exists(os.path.join(tmpshpdir, 'test_parcels_32635_reprojected_4326.shp')), 'Reprojection of shapefile failed'
         shapesplitter.splitshp()
