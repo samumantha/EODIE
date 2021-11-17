232ff632ae58bee48cd8a5627a5ec80d69d5545b
samumantha <samantha.wittke@aalto.fi>
2021-11-17 12:13:08 +0200

    preparations for packaging

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="EODIE",
+    version="1.0.0",
+    author="Samantha Wittke",
+    author_email="samantha.wittke@nls.fi",
+    description="Earth Observation Data Information Extractor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://eodie.readthedocs.io/en/latest/",
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+
+        "python", 
+        "numpy",
+        "shapely",
+        "rasterio",
+        "rasterstats",
+        "fiona",
+        "gdal",
+        "pyyaml",
+        "pytest",
+        "matplotlib"
+
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
