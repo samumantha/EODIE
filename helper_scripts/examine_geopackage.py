b4f8b7c3c5eeae9f29d1ede746b539980cae02d4
kiviarttu <arttu.kivimaki@elisanet.fi>
2022-03-31 16:12:05 +0300

    Added attribute fields to output

--- helper_scripts/examine_geopackage.py
+++ helper_scripts/examine_geopackage.py
@@ -4,10 +4,11 @@ Python script for examining GeoPackage to be used as an input for EODIE [https:/
 Inputs:
 One GeoPackage file (extension .gpkg)
 
-Output prints to terminal:
-- layer names
-- geometry types
-- feature counts 
+Output prints to terminal (for each layer):
+- layer name
+- geometry type
+- feature count 
+- attribute field names 
 
 Usage: python examine_geopackage.py path/to/geopackage.gpkg
 
@@ -30,25 +31,23 @@ gpkg_layers = [layer.GetName() for layer in gpkg]
 # Get the number of features in each layer
 layer_features = [layer.GetFeatureCount() for layer in gpkg]
 
+# Create an empty list for fields
+fieldlist = []
+
+# Loop through layers to get their fields
+for layer in gpkg:
+    # Get layer definition
+    defn = layer.GetLayerDefn()
+    # List all the fields in a file 
+    fields = [defn.GetFieldDefn(i).GetName() for i in range(defn.GetFieldCount())]
+    # Add the fields into a nested list
+    fieldlist.append(fields)
+
 # Get the geometry types of the layers in geopackage
 geometry_types = [ogr.GeometryTypeToName(gpkg.GetLayerByName(name).GetGeomType()) for name in gpkg_layers]
 
+# Print the terminal outputs
 print("\nThis GeoPackage contains the following layers:")
+# Loop through layers and print relevant information from each
 for i in range(len(gpkg_layers)):
-    print("\nLayer name: {} | Geometry type: {} | Feature count: {}".format(gpkg_layers[i], geometry_types[i], layer_features[i]))
-
-
-
-
-
-
-
-
-#print(gpkg_definitions)
-
-#print(gpkg_layers)
-
-
-
-
-
+    print("\nLayer name: {} | Geometry type: {} | Feature count: {} | Fields: {}".format(gpkg_layers[i], geometry_types[i], layer_features[i], fieldlist[i]))
