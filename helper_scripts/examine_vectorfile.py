f277a1f0807a131e931fd13bf99ae10bf159ef19
kiviarttu <arttu.kivimaki@elisanet.fi>
2022-04-05 12:08:33 +0300

    Renamed examine_shapefile to examine_vectorfile (works with shp, geojson, fgb)

---
+++ helper_scripts/examine_vectorfile.py
@@ -0,0 +1,25 @@
+"""
+
+Gives information on what fields are available in a given vectorfile
+
+input: location and name of a vectorfile
+
+output: prints available fieldnames in terminal
+
+use: python examine_shapefile.py path/to/vectorfile.*
+
+"""
+
+
+import sys
+import fiona
+
+vectorfilename = sys.argv[1]
+
+shapes = fiona.open(vectorfilename)
+
+# first feature
+first_feature = next(iter(shapes))
+
+print('Fieldnames available for the given vectorfile: ' + str(list(first_feature['properties'].keys())))
+
