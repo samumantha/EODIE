afad787a63e897723c41b3d98f8c202513cf01b6
Samantha Wittke <samantha.wittke@nls.fi>
2022-04-08 09:55:10 +0000

    add docs test

---
+++ src/eodie/src/eodie/docs.py
@@ -0,0 +1,16 @@
+"""
+
+A test file for docs
+
+"""
+
+
+class docs(object):
+    "this is test"
+    def __init__(self):
+        " prints hello"
+        print('hello')
+
+    def moi(self):
+        "prints moi"
+        print('moi')
