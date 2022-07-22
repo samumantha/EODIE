78a13b039f39e0fce5521ccbd5c1d2a64a58c869
kiviarttu <arttu.kivimaki@elisanet.fi>
2022-07-22 13:18:10 +0300

    Removed unnecessary imports, added docstring

--- src/eodie_process_new.py
+++ src/eodie_process_new.py
@@ -1,39 +1,23 @@
 #!/usr/bin/env python
-
-import glob
-import math
-import argparse
-import re
-import sys
-import os
-from eodie.extractor import Extractor
-from eodie.mask import Mask
-from eodie.index import Index
-from eodie.vectordata import VectorData
-from eodie.pathfinder import Pathfinder
-from eodie.rastervalidator_s2 import RasterValidatorS2
-from eodie.writer import Writer
 from eodie.userinput import UserInput
-from eodie.tilesplitter import TileSplitter
-from eodie.rasterdata import RasterData
-from eodie.validator import Validator
-import logging
-from datetime import datetime
-import timeit
-from dask import delayed
-from dask import compute
-import geopandas as gpd
 from eodie.workflow import Workflow
 
 def read_userinput():
+    """Read and validate userinputs.
+
+    Returns:
+    --------
+    userinput: class Userinput
+        validated Userinputs
+    """
     userinput = UserInput()      
     Validator(userinput)
     return userinput   
 
 def main():    
-    
+    """Fetch userinput and launch workflow."""
     userinput = read_userinput()  
-    process = Workflow(userinput)   
+    Workflow(userinput)   
     
 if __name__ == '__main__':
     main()
