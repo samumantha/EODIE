0c96205d5a0d4bd2bfe9ef4b20e2f6293eaa3cd0
kiviarttu <arttu.kivimaki@elisanet.fi>
2022-07-25 14:18:01 +0300

    Added docstring

--- src/eodie_process_new.py
+++ src/eodie_process_new.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+"""
+New version of EODIE_process.
+Written by Arttu Kivimäki (FGI) in July 2022 based on old eodie_process.py.
+Processing workflows for different platforms can be found in eodie/workflow.py.
+"""
+
 from eodie.userinput import UserInput
 from eodie.validator import Validator
 from eodie.workflow import Workflow
@@ -17,7 +24,7 @@ def read_userinput():
 
 def main():    
     """Fetch userinput and launch workflow."""
-    userinput = read_userinput()  
+    userinput = read_userinput()      
     Workflow(userinput)   
     
 if __name__ == '__main__':
