2fab73c9756e140422f03c355c01c4432106d8c1
kiviarttu <arttu.kivimaki@elisanet.fi>
2022-09-12 12:04:53 +0300

    Attempt on fixing docstring

--- src/eodie/workflow.py
+++ src/eodie/workflow.py
@@ -1,6 +1,5 @@
 """
-
-Class to run the actual processing based on given data platform. Based on old eodie_process.py by Samantha Wittke et al.
+Class to run the actual processing based on given data platform. Based on old eodie_process.py by Samantha Wittke et al. 
 Author: Arttu Kivimäki (FGI) - July 2022. 
 
 """
