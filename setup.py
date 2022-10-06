8cdc1ff7cca0622e419bc7e382df2a88ae7da63b
Samantha Wittke <samantha.wittke@nls.fi>
2022-10-06 10:47:31 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://eodie.readthedocs.io/en/latest/",
-    packages=setuptools.find_namespace_packages(where='src'),
+    packages=setuptools.find_namespace_packages(),
     package_data={"config":["*.yml"]},
     entry_points={
         'console_scripts': [
